dafbac8027e03cac0bcdc1fc5e92d7f668940e40
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2020-05-02 02:21:03 +0700

    Add base facebook login service class

---
+++ Scripts/BaseFacebookLoginService.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public abstract class BaseFacebookLoginService : MonoBehaviour
+{
+    public abstract void LoginWithFacebook(string userId, string accessToken, UnityAction<PlayerResult> onFinish);
+}
