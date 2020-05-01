80b19a613e5317de41adbf24119508c05831180a
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2020-05-02 02:21:31 +0700

    Implement facebook login client for web service client

---
+++ Scripts/WebServiceFacebookLogin.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(WebServiceClient))]
+public class WebServiceFacebookLogin : BaseFacebookLoginService
+{
+    private WebServiceClient webServiceClient;
+
+    private void Awake()
+    {
+        webServiceClient = GetComponent<WebServiceClient>();
+    }
+
+    public override void LoginWithFacebook(string userId, string accessToken, UnityAction<PlayerResult> onFinish)
+    {
+        var dict = new Dictionary<string, object>();
+        dict.Add("userId", userId);
+        dict.Add("accessToken", accessToken);
+        webServiceClient.PostAsDecodedJSON<PlayerResult>("/login-with-facebook", (www, result) =>
+        {
+            onFinish(result);
+        }, JsonConvert.SerializeObject(dict));
+    }
+}
