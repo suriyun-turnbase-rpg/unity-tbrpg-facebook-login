0b40f1b6150bc08402d22ca87f5efb3a2d8e208e
Insthync <ittipon.bay@gmail.com>
2021-02-15 08:56:59 +0700

    Fix wrong path

--- Scripts/WebServiceFacebookLogin.cs
+++ Scripts/WebServiceFacebookLogin.cs
@@ -19,7 +19,7 @@ public class WebServiceFacebookLogin : BaseFacebookLoginService
         var dict = new Dictionary<string, object>();
         dict.Add("userId", userId);
         dict.Add("accessToken", accessToken);
-        webServiceClient.PostAsDecodedJSON<PlayerResult>("/login-with-facebook", (www, result) =>
+        webServiceClient.PostAsDecodedJSON<PlayerResult>("login-with-facebook", (www, result) =>
         {
             onFinish(result);
         }, dict);
